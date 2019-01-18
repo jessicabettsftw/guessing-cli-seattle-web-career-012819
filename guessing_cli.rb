4d4e6e8f047ad4de0eadb03819bfb66c0f423885
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-18 23:17:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,5 +5,10 @@ def run_guessing_game()
   while keep_going 
     puts "Guess a number between 1 and 6."
     user_input = gets.chomp
+    if user_input.downcase == "exit"
+      puts "goodbye"
+    else
+      
+    end
   end
 end
